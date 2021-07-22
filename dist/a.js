dfa5084ee8625fe20239a87a23e4dec54ea1c2fe
Marcin Wosinek <marcin@wosinek.eu>
2021-07-22 07:44:08 +0200

    add build script

---
+++ dist/a.js
@@ -0,0 +1,8 @@
+import {
+  __toModule,
+  require_jquery
+} from "./chunk-TTCANJWN.js";
+
+// src/a.js
+var import_jquery = __toModule(require_jquery());
+(0, import_jquery.default)("#a").html("A file, file A");
---
+++ dist/b.js
@@ -0,0 +1,8 @@
+import {
+  __toModule,
+  require_jquery
+} from "./chunk-TTCANJWN.js";
+
+// src/b.js
+var import_jquery = __toModule(require_jquery());
+(0, import_jquery.default)("#b").html("B yourself");
